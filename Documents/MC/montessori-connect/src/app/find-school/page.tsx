b7a210b664a1a03f74bd0d09a7ecb4694e0d874c
alfred-a <49098665+alfred-a@users.noreply.github.com>
2024-09-29 02:24:04 -0400

    Delete Documents/MC directory

--- Documents/MC/montessori-connect/src/app/find-school/page.tsx
+++
@@ -1,9 +0,0 @@
-import FindSchool from '../../components/FindSchool';
-
-export default function FindSchoolPage() {
-  return (
-    <main className="h-screen">
-      <FindSchool />
-    </main>
-  );
-}
